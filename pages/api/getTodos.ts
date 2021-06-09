7790844b096d45b9a1a9242bb9b7a120a1a9826c
Kanui <marley.kanui@gmail.com>
2021-06-09 12:30:12 -0500

    auth0 - global commit 08 - added airtable table heplers to api/utils directory

--- pages/api/getTodos.ts
+++ pages/api/getTodos.ts
@@ -1,27 +1,8 @@
 // Next Types
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-// AirTable
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_ID
-);
-
-const table = base(process.env.AIRTABLE_TABLE_NAME);
-
-const getMinifiedRecord = (record) => {
-  if (!record.fields.completed) {
-    record.fields.completed = false;
-  }
-  return {
-    id: record.id,
-    fields: record.fields,
-  };
-};
-
-const getMinifiedRecords = (records) => {
-  return records.map((record) => getMinifiedRecord(record));
-};
+// AirTable Helpers
+import { table, getMinifiedRecords } from './utils/Airtable';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const records = await table.select({}).firstPage();
---
+++ pages/api/utils/Airtable.ts
@@ -0,0 +1,23 @@
+// AirTable
+const Airtable = require('airtable');
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+);
+
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+
+const getMinifiedRecord = (record) => {
+  if (!record.fields.completed) {
+    record.fields.completed = false;
+  }
+  return {
+    id: record.id,
+    fields: record.fields,
+  };
+};
+
+const getMinifiedRecords = (records) => {
+  return records.map((record) => getMinifiedRecord(record));
+};
+
+export { table, getMinifiedRecord, getMinifiedRecords };
