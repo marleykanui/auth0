58819aa0edb1be7ee4b87e3a1b50244dc3298492
Kanui <marley.kanui@gmail.com>
2021-06-09 11:30:04 -0500

    auth0 - global commit 04 - set up index page, working on navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 // Styles
 import '../styles/globals.scss';
 
+// Third Party Styles
+import '../styles/tailwind.css';
+
 // Component Level Types
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container mx-auto my-10">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
-// Styles
-import styles from '../styles/Home.module.scss';
+// Next
+import Head from 'next/head';
 
-const Auth0Landing = () => {
-  return <div className={styles.container}></div>;
+// React Types
+import { FC } from 'react';
+
+const Auth0Landing: FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Auth0 - Kanui</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Auth0 - Kanui</h1>
+      </main>
+    </div>
+  );
 };
 
 export default Auth0Landing;
